74ae6b93e44857d498461d78699244bb74a464f9
Shifatur-Rahman <rjnovel28@gmail.com>
2023-09-07 14:17:02 +0600

    img add

--- src/components/ProductType/ProductType.jsx
+++ src/components/ProductType/ProductType.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import property1 from "../../asset/images/property logo/one.png";
 import property2 from "../../asset/images/property logo/two.png";
 import property3 from "../../asset/images/property logo/three.png";
@@ -8,9 +8,30 @@ import property6 from "../../asset/images/property logo/six.png";
 import property7 from "../../asset/images/property logo/seven.png";
 import property8 from "../../asset/images/property logo/eight.png";
 import "./ProductType.css";
-import { BsBookshelf } from 'react-icons/bs';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const ProductType = () => {
+
+    useEffect(() => {
+        AOS.init({
+           offset: 120, 
+           delay: 0,
+           easing: "ease",
+           duration: 1500,
+           disable: false,
+           once: false,
+           mirror: false,
+           startEvent: 'DOMContentLoaded', 
+           animatedClassName: 'aos-animate',
+           initClassName: 'aos-init',
+           useClassNames: false,
+        });
+      }, []);
+
+
+
+
   return (
     <>
     
@@ -25,19 +46,19 @@ const ProductType = () => {
 
                       <div className="col-lg-3 col-sm-6 wow fadeInUp">
                           <a style={{ textDecoration: "none", cursor: "pointer" }} className="cat-item d-block bg-light text-center rounded p-3" >
-                              <div className="rounded p-4">
+                              <div data-aos="fade-top" className="rounded p-4">
                                   <div className="icon mb-3">
                                       <img className="img-fluid" src={property1} alt="Icon" />
                                   </div>
                                   <h6 className='propertyHeader'>Home Furnitutre</h6>
-                                  <span className='propertySpan'> 500+ Products </span>
+                                  <span className='propertySpan'> 200+ Products </span>
                               </div>
                           </a>
                       </div>
 
                       <div className="col-lg-3 col-sm-6 wow fadeInUp">
                           <a style={{ textDecoration: "none", cursor: "pointer" }} className="cat-item d-block bg-light text-center rounded p-3">
-                              <div className="rounded p-4">
+                              <div data-aos="fade-top" className="rounded p-4">
                                   <div className="icon mb-3">
                                       <img className="img-fluid" src={property2} alt="Icon" />
                                   </div>
@@ -54,7 +75,7 @@ const ProductType = () => {
                                       <img className="img-fluid" src={property3} alt="Icon" />
                                   </div>
                                   <h6 className='propertyHeader'>Industrial Furnitutre</h6>
-                                  <span className='propertySpan'>500+ Products</span>
+                                  <span className='propertySpan'>50+ Products</span>
                               </div>
                           </a>
                       </div>
@@ -66,7 +87,7 @@ const ProductType = () => {
                                       <img className="img-fluid" src={property4} alt="Icon" />
                                   </div>
                                   <h6 className='propertyHeader'>School Furnitutre</h6>
-                                  <span className='propertySpan'>500+ Products</span>
+                                  <span className='propertySpan'>100+ Products</span>
                               </div>
                           </a>
                       </div>
@@ -78,7 +99,7 @@ const ProductType = () => {
                                       <img className="img-fluid" src={property5} alt="Icon" />
                                   </div>
                                   <h6 className='propertyHeader'>Plastic Furnitutre</h6>
-                                  <span className='propertySpan'>500+ Products</span>
+                                  <span className='propertySpan'>200+ Products</span>
                               </div>
                           </a>
                       </div>
@@ -90,7 +111,7 @@ const ProductType = () => {
                                       <img className="img-fluid" src={property6} alt="Icon" />
                                   </div>
                                   <h6 className='propertyHeader'>Plastic Household</h6>
-                                  <span className='propertySpan'>500+ Products</span>
+                                  <span className='propertySpan'>300+ Products</span>
                               </div>
                           </a>
                       </div>
@@ -102,7 +123,7 @@ const ProductType = () => {
                                       <img className="img-fluid" src={property7} alt="Icon" />
                                   </div>
                                   <h6 className='propertyHeader'>Toy</h6>
-                                  <span className='propertySpan'>500+ Products</span>
+                                  <span className='propertySpan'>100+ Products</span>
                               </div>
                           </a>
                       </div>
@@ -114,7 +135,7 @@ const ProductType = () => {
                                       <img className="img-fluid" src={property8} alt="Icon" />
                                   </div>
                                   <h6 className='propertyHeader'>Upvc</h6>
-                                  <span className='propertySpan'>500+ Products</span>
+                                  <span className='propertySpan'>200+ Products</span>
                               </div>
                           </a>
                       </div>
