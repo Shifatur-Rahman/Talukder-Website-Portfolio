a8d64db3d35fb1c58ff2731cf3737b8870b1555b
Shifatur-Rahman <rjnovel28@gmail.com>
2023-09-14 11:14:22 +0600

    product link

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -7,7 +7,7 @@ import totalProduct from "../Portfolio/PortfolioImgApi.jsx";
 
 const ProductDetails = () => {
 
-  let { productId } = useParams();
+  let { id } = useParams();
   // console.log(id);
   
   return (
@@ -18,22 +18,30 @@ const ProductDetails = () => {
 
          {
           totalProduct.map((singleItem)=>{
-             console.log(singleItem.title);
-            if(singleItem.id === productId){
+            //  console.log(singleItem.id);
+            //  console.log(id);
+
+            if(singleItem.id == id){
               return(
                 <div key={singleItem.id}>
-
                   <p>{singleItem.title}</p>
+                 <img src={`${singleItem.imgSrc}`} alt="single product" />
 
                 </div>
+
+                // <p>Hello</p>
+
               )
-            } else{
-              return (
-                <div key={singleItem.id}>
-                  <p>This is not the item you're looking for.</p>
-                </div>
-              );
-            }
+            } 
+            
+            // else{
+            //   return (
+            //     <div key={singleItem.id}>
+            //       <p>This is not the item you're looking for.</p>
+            //     </div>
+            //   );
+            // }
+
           })
          }
 
