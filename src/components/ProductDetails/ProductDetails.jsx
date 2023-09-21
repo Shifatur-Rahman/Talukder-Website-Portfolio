a6c5924f5360ccc499680bf09321a5fcdf483090
Shifatur-Rahman <rjnovel28@gmail.com>
2023-09-21 10:10:51 +0600

    status add

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -14,6 +14,7 @@ import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
+import { Container } from 'react-bootstrap';
 
 const ProductDetails = ({ productQuantity, setProductQuantity, setCartProductQuantity, }) => {
 
@@ -26,6 +27,12 @@ const ProductDetails = ({ productQuantity, setProductQuantity, setCartProductQua
   };
 
   let { id } = useParams();
+
+  const [value, setValue] = React.useState('1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   return (
     <>
@@ -123,7 +130,6 @@ const ProductDetails = ({ productQuantity, setProductQuantity, setCartProductQua
                       </div>
                     </div>
 
-
                     <div className="product-description flow">
                       <p className="text-uppercase fw-700 fs-100 letter-spacing-1 Orange">
                         {/* Talukder Group of Industries */}
@@ -187,6 +193,25 @@ const ProductDetails = ({ productQuantity, setProductQuantity, setCartProductQua
                   </div>
 
                {/* Product description */}
+
+               <Container>
+
+                  <Box sx={{ width: '100%', typography: 'body1' }}>
+                    <TabContext value={value}>
+                      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                        <TabList onChange={handleChange} aria-label="lab API tabs example">
+                          <Tab label="Item One" value="1" />
+                          <Tab label="Item Two" value="2" />
+                          <Tab label="Item Three" value="3" />
+                        </TabList>
+                      </Box>
+                      <TabPanel value="1">Item One</TabPanel>
+                      <TabPanel value="2">Item Two</TabPanel>
+                      <TabPanel value="3">Item Three</TabPanel>
+                    </TabContext>
+                  </Box>
+
+                  </Container>
 
 
                 </main>
