f914a6bcb8bd33f0e92cbc9f3309bc3dc9760653
Shifatur-Rahman <rjnovel28@gmail.com>
2023-09-25 14:36:28 +0600

    store location add

--- src/components/ProductDetails/StoreLocation.jsx
+++ src/components/ProductDetails/StoreLocation.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./ProductDetails.css"
 // import { BiSolidLocationPlus } from "@react-icons/all-files/BiSolidLocationPlus";
 import { MdOutlineLocationCity } from 'react-icons/md';
+import { BsTelephoneFill } from 'react-icons/bs';
 
 
 const StoreLocation = () => {
@@ -15,14 +16,20 @@ const StoreLocation = () => {
             <MdOutlineLocationCity className='storeLocationLogo' />
             <span style={{ color: "#ff7d1a", fontSize:"25px", fontWeight:"700",marginTop:"10px" }}> Store Locations </span>
           </span> <br/>
-          <span>Store Location Content</span> <br />
-          <span>available all products</span> <br />
-          <a href='#'>Store Locator</a>
+          <span style={{color:"#595959", fontSize:"15px"}}>Store Location Content</span> <br />
+          <span style={{color:"#595959", fontSize:"15px"}}>available all products</span> <br />
+          <a style={{textDecoration:"underline", color:"#ff7d1a"}} href='#'>Store Locator</a>
         </div>
 
-      <div className='rightStore'>
-        <p>Contact us</p>
-      </div>
+        <div className='rightStore'>
+          <span>
+            <BsTelephoneFill className='storeLocationLogoContact' />
+            <span style={{ color: "#ff7d1a", fontSize: "25px", fontWeight: "700" }}> Contact us </span>
+          </span> <br />
+          <span style={{ color: "#595959", fontSize: "15px" }}>Have any question? We are here to talk! </span> <br />
+          <span style={{ color: "#595959", fontSize: "15px" }}>Let us know your queries</span> <br />
+          <a style={{ textDecoration: "underline", color: "#ff7d1a" }} href='#'>Contact Us</a>
+        </div>
 
     </div>
     
