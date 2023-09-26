72fdef033428970de6d0a8c2d02301806f14f60e
Shifatur-Rahman <rjnovel28@gmail.com>
2023-09-26 10:20:38 +0600

    product details responsive

--- src/components/ProductDetails/StoreLocation.jsx
+++ src/components/ProductDetails/StoreLocation.jsx
@@ -14,21 +14,21 @@ const StoreLocation = () => {
         <div className='leftStore'>
           <span>
             <MdOutlineLocationCity className='storeLocationLogo' />
-            <span style={{ color: "#ff7d1a", fontSize:"25px", fontWeight:"700",marginTop:"10px" }}> Store Locations </span>
+            <span className='storeHeader'> Store Locations </span>
           </span> <br/>
-          <span style={{color:"#595959", fontSize:"15px"}}>Store Location Content</span> <br />
-          <span style={{color:"#595959", fontSize:"15px"}}>available all products</span> <br />
+          <span className='storeSubHeader' style={{color:"#595959", fontSize:"15px"}}>Store Location Content</span> <br />
+          <span className='storeSubHeader' style={{color:"#595959", fontSize:"15px"}}>available all products</span> <br />
           <a style={{textDecoration:"underline", color:"#ff7d1a"}} href='#'>Store Locator</a>
         </div>
 
         <div className='rightStore'>
           <span>
             <BsTelephoneFill className='storeLocationLogoContact' />
-            <span style={{ color: "#ff7d1a", fontSize: "25px", fontWeight: "700" }}> Contact us </span>
+            <span className='storeHeader'> Contact us </span>
           </span> <br />
-          <span style={{ color: "#595959", fontSize: "15px" }}>Have any question? We are here to talk! </span> <br />
-          <span style={{ color: "#595959", fontSize: "15px" }}>Let us know your queries</span> <br />
-          <a style={{ textDecoration: "underline", color: "#ff7d1a" }} href='#'>Contact Us</a>
+          <span className='storeSubHeader' style={{ color: "#595959", fontSize: "15px" }}>Have any question? We are here to talk! </span> <br />
+          <span className='storeSubHeader' style={{ color: "#595959", fontSize: "15px" }}>Let us know your queries</span> <br />
+          <a className='storeSubHeader' style={{ textDecoration: "underline", color: "#ff7d1a" }} href='#'>Contact Us</a>
         </div>
 
     </div>
