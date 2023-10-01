7500a2e1f65953d5f33f2fe6b4c9cbc71fef6e48
Shifatur-Rahman <rjnovel28@gmail.com>
2023-10-01 15:28:59 +0600

    error page add

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import "./Error.css"
 import Error1 from "../../asset/images/Error img/error1.jpg"
 import Error2 from "../../asset/images/Error img/error2.jpg"
-import TopNavigation from '../TopNavigation/TopNavigation'
+// import TopNavigation from '../TopNavigation/TopNavigation'
 
 const Error = () => {
   return (
     <>
     
     <div className='error'>
-        <img src={Error1} alt="Error 1 Page" />
+        <img src={Error2} alt="Error 1 Page" />
     </div>
     
     </>
