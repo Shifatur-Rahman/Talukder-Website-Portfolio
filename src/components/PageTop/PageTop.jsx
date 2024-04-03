2d889f2683c2941462ec79a3255df9e37a519208
Shifatur-Rahman <rjnovel28@gmail.com>
2024-04-03 09:23:41 +0600

    status add

--- src/components/PageTop/PageTop.jsx
+++ src/components/PageTop/PageTop.jsx
@@ -5,6 +5,7 @@ import pic1 from "../../asset/images/vector img/ZigzagDiagonalCyan1.png"
 import pic2 from "../../asset/images/vector img/crossCyan .png"
 import pic3 from "../../asset/images/vector img/circleCoral2.png"
 import pic4 from "../../asset/images/vector img/TriangleCyan.png"
+import Slider from "../Slider/Slider";
 
 class PageTop extends Component {
   render() {
@@ -26,7 +27,7 @@ class PageTop extends Component {
   {/* new formula apply */}
      
   <div className="pageTopBanner">
-        <div className="vector">
+        {/* <div className="vector">
             <img data-aos="fade-right" src={pic1} alt="pic1" />
             <img data-aos="fade-right" src={pic2} alt="pic2" />
             <img data-aos="fade-right" src={pic3} alt="pic3" />
@@ -34,9 +35,11 @@ class PageTop extends Component {
             <div className="pageContent text-center">
             <p data-aos="fade-right" className="pageCover">{this.props.pageTitle}</p>
             </div>
-        </div>
+        </div> */}
+
+        <Slider />
   </div>
-  
+                
   </>
     );
   }
