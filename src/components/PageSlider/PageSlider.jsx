22a4bd3c617d12f22287fd7cdbbc87ed59449d5e
Shifatur-Rahman <rjnovel28@gmail.com>
2024-04-04 15:49:20 +0600

    status add

--- src/components/PageSlider/PageSlider.jsx
+++ src/components/PageSlider/PageSlider.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import Carousel from 'react-bootstrap/Carousel';
 import slider1 from "../../asset/images/PageTop Slider/slider3.jpg";
 import slider2 from "../../asset/images/PageTop Slider/slider1.jpg";
-import Button from 'react-bootstrap/Button';
 import "./PageSlider.css"
 
 const PageSlider = () => {
   return (
     <>
+    
     <div>
     <Carousel data-bs-theme="dark">
       <Carousel.Item className="carousel-item-fixed-height">
