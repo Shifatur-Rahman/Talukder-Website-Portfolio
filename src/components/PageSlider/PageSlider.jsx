1a9e36b640afc81a3f2b7e219ff06c25055a7635
Shifatur-Rahman <rjnovel28@gmail.com>
2024-04-15 18:01:32 +0600

    status add

--- src/components/PageSlider/PageSlider.jsx
+++ src/components/PageSlider/PageSlider.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import Carousel from 'react-bootstrap/Carousel';
-import slider1 from "../../asset/images/PageTop Slider/slider3.jpg";
-import slider2 from "../../asset/images/PageTop Slider/slider1.jpg";
+import slider1 from "../../asset/images/PageTop Slider/slider1.jpg";
+import slider2 from "../../asset/images/PageTop Slider/slider2.jpg";
+import slider3 from "../../asset/images/PageTop Slider/slider3.jpg";
 import "./PageSlider.css"
 
 const PageSlider = () => {
   return (
     <>
-    
-    
-    <Carousel data-bs-theme="dark">
+    <Carousel data-bs-theme="dark" style={{backgroundColor:"#f2f2f2"}}>
       <Carousel.Item className="carousel-item-fixed-height">
         <img
           className="d-block w-100"
@@ -27,14 +26,12 @@ const PageSlider = () => {
       <Carousel.Item className="carousel-item-fixed-height">
         <img
           className="d-block w-100"
-          src={slider1}
+          src={slider3}
           alt="Third slide"
         />
       </Carousel.Item>
     </Carousel>
-
     </>
   )
 }
-
 export default PageSlider
